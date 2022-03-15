ec2faf1e39b72f3276152d68ac39a7adb8e79944
YC Yoon <z20152002@gmail.com>
2022-03-15 23:37:58 +0900

    Dev: Member service

--- hello-jpa/src/main/java/jpabook/jpashop/service/MemberService.java
+++ hello-jpa/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,18 +2,28 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor // 롬복
 public class MemberService {
     
-    @Autowired
-    private MemberRepository memberRepository;
-    
-    // 회원 가입
+    private final MemberRepository memberRepository;
+
+//    @Autowired // 생략 가능
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    } // 생성자 주입 (여기서 RequiredArgsConstructor 대신함)
+
+   // 회원 가입
+    @Transactional // 기본값인 readOnly = false 적용
     public Long join(Member member) {
         validateDuplicateMember(member); // 중복 회원 검증
         memberRepository.save(member);
@@ -31,5 +41,9 @@ public class MemberService {
     // 회원 전체 조회
     public List<Member> findMembers() {
         return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
     }
 }
