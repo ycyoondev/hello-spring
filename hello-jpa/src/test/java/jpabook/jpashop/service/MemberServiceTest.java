2693d17f56d2b096123c281cdda8ced2095d9915
YC Yoon <z20152002@gmail.com>
2022-03-17 23:39:27 +0900

    Dev: Setting unit test env

---
+++ hello-jpa/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+
+    }
+}
