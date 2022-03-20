326c7670af916c69834cf444dbe595b833b55d54
YC Yoon <z20152002@gmail.com>
2022-03-20 09:02:21 +0900

    Dev: make test code of member join

--- hello-jpa/src/main/java/jpabook/jpashop/MemberRepository.java
+++ hello-jpa/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-@Repository
 public class MemberRepository {
 
     @PersistenceContext
--- hello-jpa/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ hello-jpa/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,15 +1,20 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.*;
 
+@WebAppConfiguration
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
@@ -17,13 +22,17 @@ public class MemberServiceTest {
 
     @Autowired
     MemberService memberService;
-
     @Autowired
     MemberRepository memberRepository;
 
     @Test
     public void 회원가입() throws Exception {
+        Member member = new Member();
+        member.setName("홍길동");
 
+        Long savedId = memberService.join(member);
+
+        Assertions.assertEquals(member, memberRepository.findOne(savedId));
     }
 
     @Test
