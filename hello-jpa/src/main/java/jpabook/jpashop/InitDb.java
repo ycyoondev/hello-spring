a5cd7420a1b06380da44bf660d6d87b0778a1949
YC Yoon <z20152002@gmail.com>
2022-03-26 22:25:39 +0900

    Dev: Data generation at run server

--- hello-jpa/src/main/java/jpabook/jpashop/InitDb.java
+++ hello-jpa/src/main/java/jpabook/jpashop/InitDb.java
@@ -1,9 +1,62 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.domain.item.Book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
 
 @Component
 @RequiredArgsConstructor
 public class InitDb {
+
+    private final InitService initService;
+
+    @PostConstruct // 마지막에 작동
+    public void init() {
+        initService.dbInit();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+        private final EntityManager em;
+        public void dbInit() {
+            Member member = createMember("userA");
+            em.persist(member);
+            Member member2 = createMember("userB");
+            em.persist(member2);
+
+            Book book = new Book();
+            book.setName("springA");
+            book.setPrice(10000);
+            book.setStockQuantity(200);
+            em.persist(book);
+
+            Book book2 = new Book();
+            book2.setName("springB");
+            book2.setPrice(20000);
+            book2.setStockQuantity(400);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+
+        private Member createMember(String name) {
+            Member member = new Member();
+            member.setName(name);
+            member.setAddress(new Address("서울", "1", "111"));
+            return member;
+        }
+    }
 }
