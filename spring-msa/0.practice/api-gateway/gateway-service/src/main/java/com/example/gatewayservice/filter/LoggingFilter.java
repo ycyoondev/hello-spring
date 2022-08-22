c040fdf40c7eb7c54e9282c5b080ac6c1319b7f0
YC Yoon <z20152002@gmail.com>
2022-08-22 17:50:35 +0900

    feat: eureka client
    유레카 클라이언트 등록
    - Gateway 등록
    - 2개 서비스 등록

--- spring-msa/0.practice/api-gateway/gateway-service/src/main/java/com/example/gatewayservice/filter/LoggingFilter.java
+++ spring-msa/0.practice/api-gateway/gateway-service/src/main/java/com/example/gatewayservice/filter/LoggingFilter.java
@@ -1,33 +1,48 @@
 package com.example.gatewayservice.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @Component
-public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
-    public GlobalFilter() {
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+    public LoggingFilter() {
         super(Config.class);
     }
 
     @Override
     public GatewayFilter apply(Config config) {
-        return ((exchange, chain) -> {
+//        return ((exchange, chain) -> {
+//            ServerHttpRequest request = exchange.getRequest();
+//            ServerHttpResponse response = exchange.getResponse();
+//            System.out.println("Global Filter baseMessage -> " + config.getBaseMassage());
+//
+//            if (config.isPreLogger()) {
+//                System.out.println("request.getId() = " + request.getId());
+//            }
+//
+//            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//                System.out.println("Custom POST filter: response code -> " + response.getStatusCode());
+//            }));
+//        });
+        return new OrderedGatewayFilter((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
-            System.out.println("Global Filter baseMessage -> " + config.getBaseMassage());
+            System.out.println("Logging Filter baseMessage -> " + config.getBaseMassage());
 
             if (config.isPreLogger()) {
                 System.out.println("request.getId() = " + request.getId());
             }
 
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                System.out.println("Custom POST filter: response code -> " + response.getStatusCode());
+                System.out.println("Logging POST filter: response code -> " + response.getStatusCode());
             }));
-        });
+        }, Ordered.HIGHEST_PRECEDENCE);
     }
 
     public static class Config {
