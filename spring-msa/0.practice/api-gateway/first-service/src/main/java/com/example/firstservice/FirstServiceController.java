7428e1a3cee4b1869d38bedeb26dcd90e5aee12b
YC Yoon <z20152002@gmail.com>
2022-08-22 21:42:57 +0900

    feat: logging eureka client port
    유레카 클라이언트 포트 출력

--- spring-msa/0.practice/api-gateway/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ spring-msa/0.practice/api-gateway/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,15 +1,26 @@
 package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
+
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -23,7 +34,10 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First service";
+    public String check(HttpServletRequest request) {
+        log.info("server port = {}", request.getServerPort());
+//        return "Hi, there. This is a message from First service";
+        return String.format("Hi, there. This is a message from First service on port %s",
+                env.getProperty("local.server.port"));
     }
 }
