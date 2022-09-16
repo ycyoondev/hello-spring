b0bc14abf57d0af0b0bbf791d33bd7b19bd52198
YC Yoon <z20152002@gmail.com>
2022-09-16 22:22:12 +0900

    fix: Security setting
    Spring Security 설정

---
+++ spring-msa/1.toy-ecommerce/user/src/main/java/com/example/user/security/WebSecurity.java
@@ -0,0 +1,22 @@
+package com.example.user.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurity {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable(); // csrf 무시
+        http.authorizeRequests().antMatchers("/users/**").permitAll();
+
+        return http.build();
+    }
+}
