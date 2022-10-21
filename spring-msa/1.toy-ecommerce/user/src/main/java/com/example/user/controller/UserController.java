e45054f6b8edd2649591f250574ea3aa767f9f13
YC Yoon <z20152002@gmail.com>
2022-10-21 20:53:17 +0900

    feat: Eureka registration

--- spring-msa/1.toy-ecommerce/user/src/main/java/com/example/user/controller/UserController.java
+++ spring-msa/1.toy-ecommerce/user/src/main/java/com/example/user/controller/UserController.java
@@ -5,21 +5,24 @@ import com.example.user.service.UserService;
 import com.example.user.vo.Greeting;
 import com.example.user.vo.RequestUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/")
+@RequestMapping("/user-service")
 public class UserController {
 
     private final UserService userService;
     private final Greeting greeting;
+    private final Environment env;
 
     @GetMapping("/hearth_check")
     public String status() {
-        return "It's Working in User Service";
+        return String.format("It's Working in User Service on Port %s",
+                env.getProperty("local.server.port"));
     }
 
     @GetMapping("/welcome")
